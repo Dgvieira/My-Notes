729a5647e6b4b552ad3b6572db12b04910167d34
Douglas Vieira <102380160+Dgvieira@users.noreply.github.com>
2023-10-04 23:22:39 -0300

    Update db.php

--- Config/db.php
+++ Config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_name = "my_notes";
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
 
-    $conn = new PDO("mysql:dbname=". $db_name . ";host=" . $db_host, $db_user, $db_pass);
+$conn = new PDO("mysql:dbname=". $db_name . ";host=" . $db_host, $db_user, $db_pass);
